772e63d7d8258c702ff9f36f4ecd64155f315a05
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-08-18 14:55:13 -0300

    feat: configure static routes for '/', '/users' and '/cards'

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const users = require('./users.json');
+const cards = require('./cards.json');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(404).send({ message: 'A solicitação não foi encontrada' });
+});
+
+app.get('/users', (req, res) => {
+  res.send({ users });
+});
+
+app.get('/cards', (req, res) => {
+  res.send({ cards });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
