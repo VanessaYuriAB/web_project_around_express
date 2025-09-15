3fa0d28466193a0956ceba7c1a859d80228e6d7b
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-09-15 09:36:06 -0300

    feat: add new routes for users and cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,15 @@
 const cardsRouter = require('express').Router();
 
-const { getCards, createCard, deleteCardById } = require('../controllers/cards');
+const {
+  getCards, createCard, deleteCardById, likeCard, unlikeCard,
+} = require('../controllers/cards');
 
 // rotas para cartões
 cardsRouter.get('/', getCards);
 cardsRouter.post('/', createCard);
 cardsRouter.delete('/:cardId', deleteCardById);
+cardsRouter.put('/:cardId/likes', likeCard);
+cardsRouter.delete('/:cardId/likes', unlikeCard);
 
 // exporta o roteador de cartões
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 const usersRouter = require('express').Router();
 
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers, getUserById, createUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 // rotas para usuários
 usersRouter.get('/', getUsers);
 usersRouter.get('/:userId', getUserById);
 usersRouter.post('/', createUser);
+usersRouter.patch('/me', updateUser);
+usersRouter.patch('/me/avatar', updateAvatar);
 
 // exporta o roteador de usuários
 module.exports = usersRouter;
