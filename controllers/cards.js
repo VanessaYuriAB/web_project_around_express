4e53fb2ded583a157f70ded363d152611ce5756d
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-09-15 09:54:45 -0300

    feat: implement likeCard and unlikeCard controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -35,8 +35,44 @@ const deleteCardById = async (req, res) => {
   res.send({ data: cardToDelete });
 };
 
+// O manipulador de solicitação likeCard, por _id do cartão
+// Curtida, por _id do usuário (em req.user._id)
+// Erros: Not found, Cast ou Internal server
+const likeCard = async (req, res) => {
+  const likedCard = await Card.findByIdAndUpdate(
+    req.params.cardId,
+    // Adiciona o _id do usuário ao array, se ainda não existir
+    { $addToSet: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  ).orFail(() => {
+    const err = new Error('Erro ao curtir cartão, Recurso não encontrado: não existe cartão com o id solicitado');
+    err.name = 'NotFoundError';
+    throw err;
+  });
+  res.send({ data: likedCard });
+};
+
+// O manipulador de solicitação dislikeCard, por _id do cartão
+// Descurtida, por _id do usuário (em req.user._id)
+// Erros: Not found, Cast ou Internal server
+const unlikeCard = async (req, res) => {
+  const unlikedCard = await Card.findByIdAndUpdate(
+    req.params.cardId,
+    // Remove o _id do usuário do array
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  ).orFail(() => {
+    const err = new Error('Erro ao descurtir cartão, Recurso não encontrado: não existe cartão com o id solicitado');
+    err.name = 'NotFoundError';
+    throw err;
+  });
+  res.send({ data: unlikedCard });
+};
+
 module.exports = {
   getCards: handleAsync(getCards),
   createCard: handleAsync(createCard),
   deleteCardById: handleAsync(deleteCardById),
+  likeCard: handleAsync(likeCard),
+  unlikeCard: handleAsync(unlikeCard),
 };
